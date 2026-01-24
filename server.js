d5c2850df40c348e3e764a8805d578f85dac02d1
Atharv1116 <atharvanikhade94@gmail.com>
2026-01-24 11:45:48 +0530

    cors

--- server.js
+++ server.js
@@ -41,7 +41,12 @@ const io = new Server(server, {
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
